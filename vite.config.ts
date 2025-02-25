1794e88440b58678b195d1c3996f491626c07350
charliej213 <charlie.jxcques@gmail.com>
2025-02-25 19:50:01 +0000

    uplaoding and running image from github container registry

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/setupTests.ts'],
     coverage: {
-      reporter: ['text', 'json-summary'],
+      reporter: ['text', 'json-summary', 'json', 'html'],
     }
   },
 })
